3181afae30d6bb9083e0c159768767c15ede7f46
Levy <cailiwei712@163.com>
2014-01-18 21:14:18 +0800

    Add string convert

--- ubuntu/test/main.c
+++ ubuntu/test/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <malloc.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
@@ -188,7 +189,47 @@ static void testFunc(struct param_dev *cmd, int size)
 	}
 }
 
+static void stringConvert(unsigned char *src, unsigned char *dst) 
+{
+	char *rt = dst;
+	char *ps = src;
+
+	char *temp[10];
+	int cnt = 0;
+	int lenth = strlen(src);
+	int i=0, j=0, num=0;
+
+	temp[cnt++] = ps;
+	while (lenth > 0) {
+		if (*ps++ == '.') {
+			temp[cnt++] = ps;
+			num = 0;
+		} else {
+			num++;
+		}
+		lenth--;
+	}
+
+	cnt--;
+	ps = ps - num;
+
+	do {
+		j = strlen(temp[cnt]);
+		while ((j--) && (*(temp[cnt]) != '.')) {
+			rt[i++] = *(temp[cnt]++);
+		}
+		rt[i++] = '.';
+	} while ((--cnt) >= 0);
+
+	rt[--i] = '\0';
+}
+
 void main()
 {
-	testFunc(nt35516_display_off_cmds, ARRAY_SIZE(nt35516_display_off_cmds));
+//	testFunc(nt35516_display_off_cmds, ARRAY_SIZE(nt35516_display_off_cmds));
+	unsigned char A[] = "182.89.80.100";
+	unsigned char B[20];
+	printf("A:%s\n", A);
+	stringConvert(A, B);
+	printf("B:%s\n", B);
 }
